3051c5745d798fb78f1bfdc62562045bcae8f47b
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-09-12 22:00:19 +0700

    add SocialLinks + Rename Cv To Porto

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,42 @@
-import React from 'react'
-import Profpic from '../assets/IMG_0876.JPG'
-import {MdOutlineKeyboardArrowRight }from 'react-icons/md'
-
-
+import React from "react";
+import Profpic from "../assets/IMG_0876.JPG";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 const Home = () => {
   return (
-    <div name="home" className="h-screen flex w-full bg-gradient-to-b from-black to bg-gray-800">
-
-       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+    <div
+      name="home"
+      className="h-screen flex w-full bg-gradient-to-b from-black to bg-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
-            <h2 className="text-4xl sm:text-7xl font-bold text-violet-400 drop-shadow-lg">
-                I'm Informatics Student
-            </h2>
-            <p className="text-gray-300 py-4 max-w-md">
-                Currently Studying Web Programming Focusing On Backend using Laravel and Node JS. This is my attempt
-                to learn Front End Development.
-            </p>
-            <div>
-                <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
-                    CV
-                    <span className="group-hover:rotate-90 duration-500">
-                        <MdOutlineKeyboardArrowRight size={25} className="ml-1"/>
-                    </span>
-                </button>
-            </div>
+          <h2 className="text-4xl sm:text-7xl font-bold text-violet-400 drop-shadow-lg">
+            I'm Informatics Student
+          </h2>
+          <p className="text-gray-300 py-4 max-w-md">
+            Currently Studying Web Programming Focusing On Backend using Laravel
+            and Node JS. This is my attempt to learn Front End Development.
+          </p>
+          <div>
+            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-500">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </button>
+          </div>
         </div>
 
         <div>
-            <img src={Profpic} alt="My Profile" className="rounded-xl  mx-auto w-4/12 md:w-6/12"/>
+          <img
+            src={Profpic}
+            alt="My Profile"
+            className="rounded-xl  mx-auto w-4/12 md:w-6/12"
+          />
         </div>
-       </div>
-       </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,74 +1,71 @@
-import React, { useState } from 'react'
-import {FaBars,FaTimes}  from 'react-icons/fa' 
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
-
-  const  [nav,setNav] = useState(false )
-  const links=[
+  const [nav, setNav] = useState(false);
+  const links = [
     {
       id: 1,
-      link: 'home',
+      link: "home",
     },
     {
       id: 2,
-      link: 'about',
+      link: "about",
     },
     {
       id: 3,
-      link: 'portfolio',
+      link: "portfolio",
     },
     {
       id: 4,
-      link: 'experience',
+      link: "experience",
     },
     {
       id: 5,
-      link: 'contact',
+      link: "contact",
     },
-  ]
+  ];
 
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white fixed bg-black ">
-        <div>
-            <h1 className="text-6xl text-violet-500 font-signature ml-2">sugab</h1>
-        </div>
+      <div>
+        <h1 className="text-6xl text-violet-500 font-signature ml-2">sugab</h1>
+      </div>
 
-        <ul className="hidden md:flex">
-
-            {links.map(({id,link}) => (
-           
-              <li key={id} 
-              className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 "
-              >
-              {link}
-              </li>
-
-            ))}
-
-        </ul>
-        <div onClick={() => 
-          setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden">
-          {nav ? <FaTimes size={30}/> : <FaBars size={30}/>}
-        </div>
-
-          {nav && (
-            <ul 
-          className="flex flex-col justify-center items-center absolute top-0 left-0 w-full 
-          h-screen bg-gradient-to-b from from-black to-gray-800 text-gray-400">
-            {links.map(({id,link}) => (
-           
-            <li key={id} 
-            className="px-4 cursor-pointer capitalize font-medium text py-6 text-4xl">
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => (
+          <li
+            key={id}
+            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 "
+          >
             {link}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul
+          className="flex flex-col justify-center items-center absolute top-0 left-0 w-full 
+          h-screen bg-gradient-to-b from from-black to-gray-800 text-gray-400"
+        >
+          {links.map(({ id, link }) => (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize font-medium text py-6 text-4xl"
+            >
+              {link}
             </li>
-
-            ))}
-            
-          </ul>
-          )}
-
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Navbar
+export default Navbar;
