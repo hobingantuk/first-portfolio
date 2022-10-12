83e4f20d9593514e79da80f5267359bbed044aba
Bagus Nugra <bagusnugrahaxd@gmail.com>
2022-10-12 14:08:01 +0700

    Fixing experience. there are still bugs in the phone screen size

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,15 +4,83 @@ import Git from "../assets/Technologies/Git.png";
 import css from "../assets/Technologies/css.png";
 import github from "../assets/Technologies/github.png";
 import laravel from "../assets/Technologies/laravel.png";
-import javascipt from "../assets/Technologies/javascript.png";
+import javascript from "../assets/Technologies/javascript.png";
 import postman from "../assets/Technologies/postman.png";
-import react from "../assets/Technologies/react.png";
 import tailwind from "../assets/Technologies/tailwind.png";
 import node from "../assets/Technologies/node.png";
 import reactImage from "../assets/Technologies/react.png";
 import npm from "../assets/Technologies/npm.png";
 
 const Experience = () => {
+  const tech = [
+    {
+      id: 1,
+      src: html,
+      title: "HTML",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 2,
+      src: css,
+      title: "CSS",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 3,
+      src: javascript,
+      title: "Javascipt",
+      style: "shadow-yellow-500",
+    },
+    {
+      id: 4,
+      src: reactImage,
+      title: "React",
+      style: "shadow-blue-500",
+    },
+    {
+      id: 5,
+      src: node,
+      title: "Node.JS",
+      style: "shadow-green-500",
+    },
+    {
+      id: 6,
+      src: laravel,
+      title: "Laravel",
+      style: "shadow-red-500",
+    },
+    {
+      id: 7,
+      src: tailwind,
+      title: "tailwind",
+      style: "shadow-cyan-500",
+    },
+    {
+      id: 8,
+      src: postman,
+      title: "Postman",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 9,
+      src: npm,
+      title: "NPM",
+      style: "shadow-red-500",
+    },
+    {
+      id: 10,
+      src: Git,
+      title: "Git",
+      style: "shadow-orange-500",
+    },
+    {
+      id: 11,
+      src: github,
+      title: "Github",
+      style: "shadow-gray-500",
+    },
+  ];
+
   return (
     <div
       name="experience"
@@ -27,10 +95,15 @@ const Experience = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          <div className="shadow-md hover:scale-105 duration-150 py-2 rounded-lg">
-            <img src={html} alt="html" className="w-20 mx-auto" />
-            <p className="mt-4">HTML</p>
-          </div>
+          {tech.map(({ id, src, title, style }) => (
+            <div
+              key={id}
+              className={`shadow-md hover:scale-105 duration-150 py-2 rounded-lg ${style}`}
+            >
+              <img src={src} alt="html" className="w-20 mx-auto" />
+              <p className="mt-4">{title}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
